6b1b8b3a7f94f4802022f2a4df7ef48ccc02bd19
lalejandroorozcov-sketch <l.alejandro.orozco.v@gmail.com>
2026-03-03 14:00:08 +0100

    Created GET-request for todos.

--- Backend/server.js
+++ Backend/server.js
@@ -16,6 +16,37 @@ app.use(express.json())
 
 const PORT = 8080
 
+app.get('/getTodos', async (req, res) => {
+
+    try {
+
+        const todoCollection = db.collection('Todos')
+        const snapshot = await todoCollection.get()
+
+        if (snapshot.empty) {
+
+            return res.status(200).json([])
+        }
+
+        const todos = []
+
+        snapshot.forEach((todo) => {
+            todos.push({
+                id: todo.id,
+                ...todo.data()
+            })
+        })
+
+        res.status(200).json(todos)
+
+    } catch (error) {
+
+        res.status(500).send('Somthing is wrong in the server, try again later.')
+
+    }
+
+})
+
 app.listen(PORT, () => {
     console.log(`Servern körs på https://localhost:${PORT}`)
 })
