1aedaddc0c7d4561ecc5b55f7512ce9d9206e3ab
lalejandroorozcov-sketch <l.alejandro.orozco.v@gmail.com>
2026-03-04 20:09:21 +0100

    Created route for deleting todos

--- Backend/server.js
+++ Backend/server.js
@@ -16,6 +16,24 @@ app.use(express.json())
 
 const PORT = 8080
 
+app.delete('/deleteTodo/:id', async (req, res) => {
+
+    try {
+
+        const todoID = req.params.id
+
+        await db.collection('Todos').doc(todoID).delete()
+
+        res.status(200).send('Delete suecces.')
+
+    } catch (error) {
+
+        res.status(500).send('Could not delete todo.')
+
+    }
+
+})
+
 app.listen(PORT, () => {
     console.log(`Servern körs på https://localhost:${PORT}`)
 })
